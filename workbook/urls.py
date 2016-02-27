b73bef928efc16bc5fc2408152924ca5b622d7d7
Manoj Kumar Purushothaman <manoj.k@plancess.com>
2016-02-27 12:08:07 +0530

    better redirection

--- workbook/urls.py
+++ workbook/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import redirect
+from django.views.generic import RedirectView
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'workbook.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', redirect)
+    url(r'^$', RedirectView.as_view(url='/static/index.html')),
 ]
--- workbook/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def redirect(request):
-	return HttpResponse("Loading..<script>window.location='/static/index.html';</script>")
